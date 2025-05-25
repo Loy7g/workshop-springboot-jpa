8a350a07174d83584bc25de0c2ec305ec7adc175
Maximus <lianthg07@gmail.com>
2025-05-25 01:16:00 -0300

    Implements Subtotal & Total methods

--- src/main/java/com/projetojavaworkshop/work/entities/Order.java
+++ src/main/java/com/projetojavaworkshop/work/entities/Order.java
@@ -93,6 +93,13 @@ public class Order implements Serializable {
         return items;
     }
 
+    public double getTotal(){
+        double sum = 0.0;
+        for (OrderItem x : items) {
+            sum += x.getSubTotal();
+        }
+        return sum;
+    }
 
 
 
